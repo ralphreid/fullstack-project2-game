bbd5d8315b92a58711a4fb0cf590820e092fe08b
Ralph <beresfordjunior@me.com>
2015-04-16 14:35:14 +0100

    updated bootstrap

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -7,10 +7,3 @@ pip install oauth2client
 pip install requests
 pip install httplib2
 su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
-
-vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
-echo -e $vagrantTip > /etc/motd
-
